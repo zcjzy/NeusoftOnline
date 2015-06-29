e98576e8a7ebb1b2ce83033ab2f095e1912485b5
zcj <zcj@Lab304deiMac-8.local>
2015-06-29 10:05:46 +0800

    1.0

---
+++ NeuSoftOnline/NeuSoftOnline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeuSoftOnline
+//
+//  Created by zcj on 15/6/29.
+//  Copyright (c) 2015年 zcj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
